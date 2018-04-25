f92af1be8398f759325bc66accba98e9ae53adea
JifeiHe <33614387+JifeiHe@users.noreply.github.com>
2018-04-25 14:28:59 -0400

    Add files via upload

---
+++ Similarity/prediction_layer/20170425vgg_1000_save_plot.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 25 12:19:29 2018
+
+@author: jifeihe
+"""
+
+from vgg_1000_simi import simi_loop
+from matplotlib.backends.backend_pdf import PdfPages
+#for i in range(20):
+#    simi_loop(i)
+data_dir = '/Users/jifeihe/Desktop/capstone/save_results'
+os.chdir(data_dir)    
+    
+pp = PdfPages('multipage.pdf')
+for i in range(20):
+    fig = simi_loop(i)
+    fig.savefig(pp, format='pdf')
+pp.close()
